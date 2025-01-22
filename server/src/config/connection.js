1ec9df0be73b8115f114c1ffce52ed47e70b0a59
Ilan Néstor Angeles Rodriguez <ilanangelesrodriguez@gmail.com>
2025-01-22 07:09:02 -0500

    feat: agregar configuracion para despliegue a Vercel

--- server/src/config/connection.js
+++ server/src/config/connection.js
@@ -2,7 +2,17 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
 dotenv.config();
-const dbUri = process.env.DB_URl;
+
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+const dbHost = process.env.DB_HOST;
+const dbName = process.env.DB_NAME;
+
+if (!dbUser || !dbPassword || !dbHost || !dbName) {
+    throw new Error('Las variables de entorno de la base de datos no están definidas correctamente');
+}
+
+const dbUri = `mongodb+srv://${dbUser}:${dbPassword}@${dbHost}/?retryWrites=true&w=majority&appName=${dbName}`;
 
 const connectDB = async () => {
     try {
