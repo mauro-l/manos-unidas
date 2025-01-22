0cc13e808610cab6ebda2612803f8acc40c7a8f6
Ilan Néstor Angeles Rodriguez <ilanangelesrodriguez@gmail.com>
2025-01-22 01:03:04 -0500

    feat: agregar ruta de login

--- server/index.js
+++ server/index.js
@@ -19,7 +19,16 @@ connectDB()
 
 
 app.get('/', (req, res) => {
-    res.send('Hola Mundo!');
+    res.json({
+        message: "Bienvenido a la API de Gestión de Voluntariado - Manos Unidas",
+        version: "1.0",
+        description: "Esta API proporciona servicios para la gestión de voluntariado, incluyendo la administración de organizaciones y voluntarios.",
+        documentation: process.env.API_DOCUMENTATION_URL || "http://localhost:3000/api-docs",
+        endpoints: [
+            "/v1/api/login",
+            "/v1/api/voluntarios"
+        ]
+    });
 });
 
 app.use('/v1/api', routes);
