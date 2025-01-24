53dd2ce555c3951b23e834af2ac54304d39413d6
Nelida <neessu2023@gmail.com>
2025-01-24 13:43:19 -0300

    feat: add dynamic card rendering for activities from JSON in React

--- client/src/components/common/Card.jsx
+++ client/src/components/common/Card.jsx
@@ -1,33 +1,45 @@
-const Card = () => {
+import {Link} from "react-router-dom"
+
+const Card = ({ activity }) => {
+  const {
+    titulo,
+    descripcion,
+    fundacion,
+    ubicacion,
+    categoria,
+    /* tareas, */
+  } = activity;
+
+ /*  const tareasList = tareas ? tareas.split("\n") : []; */
+
   return (
-    <div className="card bg-base-100  shadow-xl mx-auto ">
+    <div className="card bg-base-100 shadow-xl mx-auto">
       <figure>
         <img
-          className=" h-36 w-96"
+          className="h-36 w-96"
           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5kp0xUTq4sBK1q-McB-ZgaDGzrzOKymk4_A&s"
-          alt="Imagen"
+          alt={titulo}
         />
       </figure>
       <div className="card-body p-6 gap-4">
-        <div className="inline-flex items-center text-[#818181] border-[#818181] gap-2 ">
-          <div className="badge badge-outline ">CATEGORÍA DE VOLUNTARIADO</div>
-          <div className="badge badge-outline ">OTRA</div>
+        <div className="inline-flex items-center text-[#818181] border-[#818181] gap-2">
+          <div className="badge badge-outline">{categoria}</div>
         </div>
-        <h2 className="card-title font-bold text-2xl leading-6 ">
-          Título del voluntariado (máximo 2 o 3 líneas)
-        </h2>
-        <div className="inline-flex gap-2 text-[#515151] text-sm ">
-          <a className="link font-bold "> Fundación organizadora </a>
-          <p className="border-l-2 border-[#D1D5DB] ps-2"> Ciudad, País</p>
+        <h2 className="card-title font-bold text-2xl leading-6">{titulo}</h2>
+        <div className="inline-flex gap-2 text-[#515151] text-sm">
+          <a className="link font-bold text-nowrap">{fundacion}</a>
+          <p className="border-l border-[#D1D5DB] ps-2 ">{ubicacion.pais}, {ubicacion.ciudad}  </p>
         </div>
-        <p >
-          Descripción de la actividad y las tareas: Lorem ipsum dolor sit amet,
-          consectetur adipiscing elit.{" "}
-        </p>
+        <p>{descripcion}</p>
+       {/*  <ul className="list-disc list-inside mt-4 text-[#515151]">
+          {tareasList.map((tar, index) => (
+            <li key={index}>{tar}</li>
+          ))}
+        </ul> */}
         <div className="card-actions justify-end"></div>
-        <button className="btn btn-outline font-bold border-[#D1D5DB]  ">
+        <Link role="button" className="btn btn-outline font-bold border-[#D1D5DB]">
           Conocer más
-        </button>
+        </Link>
       </div>
     </div>
   );
--- client/src/pages/volunteering/Activities.jsx
+++ client/src/pages/volunteering/Activities.jsx
@@ -1,16 +1,19 @@
-import Card from "../../components/common/Card.jsx"
+import Banner from "../../components/common/Banner.jsx";
+import ActivitiesList from "../../components/features/activities/activitiesList.jsx";
 
 
 const Activities = () => {
   return (
-    
-    <div className="m-4 flex justify-center">
-     
-    <Card>
+    <div className=" flex flex-col justify-center">
+      <Banner>
+        <h3 className="text-3xl leading-7">Explora las oportunidades de voluntariado que tenemos para ti</h3>
+      </Banner>
+      <div className="p-4">
         
-    </Card>
+        <ActivitiesList ></ActivitiesList>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Activities
+export default Activities;
