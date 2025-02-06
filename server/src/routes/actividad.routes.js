3339bae2473d8aa49c78d777ca3d6f9278232184
Mauro Laime <maurol.dev@gmail.com>
2025-02-06 15:46:42 -0300

    add populate

--- server/src/routes/actividad.routes.js
+++ server/src/routes/actividad.routes.js
@@ -1,19 +1,21 @@
-import express from 'express';
+import express from "express";
 import {
-    createActividad,
-    getAllActividades,
-    getActividadById,
-    updateActividad,
-    deleteActividad
-} from '../controllers/actividad.Controller.js';
-import validateId from '../config/validate_id.middleware.js';
+  createActividad,
+  getAllActividades,
+  getActividadById,
+  updateActividad,
+  deleteActividad,
+  getActividadesByFundacionId,
+} from "../controllers/actividad.Controller.js";
+import validateId from "../config/validate_id.middleware.js";
 
 const router = express.Router();
 
-router.get('/', getAllActividades);
-router.get('/:id', validateId, getActividadById);
-router.post('/', createActividad);
-router.put('/:id', validateId, updateActividad);
-router.delete('/:id', validateId, deleteActividad);
+router.get("/", getAllActividades);
+router.get("/:id", validateId, getActividadById);
+router.post("/", createActividad);
+router.put("/:id", validateId, updateActividad);
+router.delete("/:id", validateId, deleteActividad);
+router.get("/fundacion/:id", getActividadesByFundacionId);
 
 export default router;
