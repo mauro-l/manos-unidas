fd2d32df1676f2bc1bf498a87697b92de4835fc1
Mauro Laime <maurol.dev@gmail.com>
2025-02-07 12:10:05 -0300

    fixed inscriptionroutes and add populate

--- server/src/controllers/inscripcion.controller.js
+++ server/src/controllers/inscripcion.controller.js
@@ -1,72 +1,124 @@
-import Inscripcion from '../models/inscripcion.model.js';
+import Inscripcion from "../models/inscripcion.model.js";
 
 // Obtener todas las inscripciones
 export const getAllInscripciones = async (req, res) => {
-    try {
-        const inscripciones = await Inscripcion.find();
-        res.status(200).json(inscripciones);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
+  try {
+    const inscripciones = await Inscripcion.find().populate(
+      "actividad_id",
+      "titulo fecha_inicio fecha_fin voluntarios_inscriptos cupo_maximo"
+    );
+
+    res.status(200).json(inscripciones);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
-// Obtener una inscripción por ID
+// Obtener una inscripción por ID con datos de la actividad
 export const getInscripcionById = async (req, res) => {
-    try {
-        const inscripcion = await Inscripcion.findById(req.params.id);
-        if (!inscripcion) {
-            return res.status(404).json({ message: 'Inscripción no encontrada' });
-        }
-        res.status(200).json(inscripcion);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
+  try {
+    const inscripcion = await Inscripcion.findById(req.params.id).populate(
+      "actividad_id",
+      "titulo fecha_inicio fecha_fin voluntarios_inscriptos cupo_maximo"
+    );
+
+    if (!inscripcion) {
+      return res.status(404).json({ message: "Inscripción no encontrada" });
     }
+
+    res.status(200).json(inscripcion);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 // Crear una nueva inscripción
 export const createInscripcion = async (req, res) => {
-    const { voluntario_id, actividad_id } = req.body;
+  const { voluntario_id, actividad_id } = req.body;
 
-    try {
-        const newInscripcion = new Inscripcion({
-            voluntario_id,
-            actividad_id,
-            fecha_inscripcion: new Date()
-        });
+  try {
+    const newInscripcion = new Inscripcion({
+      voluntario_id,
+      actividad_id,
+      fecha_inscripcion: new Date(),
+    });
 
-        const savedInscripcion = await newInscripcion.save();
+    const savedInscripcion = await newInscripcion.save();
 
-        res.status(201).json(savedInscripcion);
-    } catch (error) {
-        console.error("Error al crear inscripción:", error); // Log del error
-        res.status(500).json({ message: 'Error al crear inscripción', error: error.message }); // Respuesta con más detalle
-    }
+    res.status(201).json(savedInscripcion);
+  } catch (error) {
+    console.error("Error al crear inscripción:", error); // Log del error
+    res
+      .status(500)
+      .json({ message: "Error al crear inscripción", error: error.message }); // Respuesta con más detalle
+  }
 };
 
 // Actualizar una inscripción por id
 export const updateInscripcion = async (req, res) => {
-    try {
-        const updatedInscripcion = await Inscripcion.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true }); // runValidators para validar las actualizaciones
-        if (!updatedInscripcion) {
-            return res.status(404).json({ message: 'Inscripción no encontrada' });
-        }
-        res.status(200).json(updatedInscripcion);
-    } catch (error) {
-        console.error("Error al actualizar inscripción:", error); // Log del error
-        res.status(500).json({ message: 'Error al actualizar inscripción', error: error.message }); // Respuesta con más detalle
+  try {
+    const updatedInscripcion = await Inscripcion.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true, runValidators: true }
+    ); // runValidators para validar las actualizaciones
+    if (!updatedInscripcion) {
+      return res.status(404).json({ message: "Inscripción no encontrada" });
     }
+    res.status(200).json(updatedInscripcion);
+  } catch (error) {
+    console.error("Error al actualizar inscripción:", error); // Log del error
+    res.status(500).json({
+      message: "Error al actualizar inscripción",
+      error: error.message,
+    }); // Respuesta con más detalle
+  }
 };
 
 // Eliminar una inscripción por ID
 export const deleteInscripcion = async (req, res) => {
-    try {
-        const deletedInscripcion = await Inscripcion.findByIdAndDelete(req.params.id);
-        if (!deletedInscripcion) {
-            return res.status(404).json({ message: 'Inscripción no encontrada' });
-        }
-        res.status(200).json({ message: 'Inscripción eliminada' });
-    } catch (error) {
-        console.error("Error al eliminar inscripción:", error); // Log del error
-        res.status(500).json({ message: 'Error al eliminar inscripción', error: error.message }); // Respuesta con más detalle
+  try {
+    const deletedInscripcion = await Inscripcion.findByIdAndDelete(
+      req.params.id
+    );
+    if (!deletedInscripcion) {
+      return res.status(404).json({ message: "Inscripción no encontrada" });
     }
+    res.status(200).json({ message: "Inscripción eliminada" });
+  } catch (error) {
+    console.error("Error al eliminar inscripción:", error); // Log del error
+    res
+      .status(500)
+      .json({ message: "Error al eliminar inscripción", error: error.message }); // Respuesta con más detalle
+  }
 };
+
+export const getInscripcionesByActividadId = async (req, res) => {
+  try {
+    // Validar que el ID sea un ObjectId válido
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+      return res.status(400).json({ message: "ID de actividad no válido" });
+    }
+
+    const actividadObjectId = new mongoose.Types.ObjectId(req.params.id);
+
+    // Buscar todas las inscripciones que coincidan con el ID de la actividad
+    const inscripciones = await Inscripcion.find({
+      actividad_id: actividadObjectId,
+    })
+      .populate("voluntario_id")
+      .populate("actividad_id");
+
+    if (!inscripciones.length) {
+      return res
+        .status(404)
+        .json({ message: "No hay inscripciones para esta actividad" });
+    }
+
+    res.status(200).json(inscripciones);
+  } catch (error) {
+    console.error("Error al obtener inscripciones por actividad:", error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
--- server/src/routes/inscripcion.routes.js
+++ server/src/routes/inscripcion.routes.js
@@ -1,26 +1,41 @@
-import express from 'express';
-import Joi from 'joi';
-import { getAllInscripciones, getInscripcionById, createInscripcion, updateInscripcion, deleteInscripcion } from '../controllers/inscripcion.controller.js';
+import express from "express";
+import Joi from "joi";
+import {
+  getAllInscripciones,
+  getInscripcionById,
+  createInscripcion,
+  updateInscripcion,
+  deleteInscripcion,
+  getInscripcionesByActividadId,
+} from "../controllers/inscripcion.controller.js";
 
 const router = express.Router();
 
 const validateInscripcionData = (req, res, next) => {
-    const schema = Joi.object({
-        voluntario_id: Joi.string().required(),
-        actividad_id: Joi.string().required()
-    });
+  const schema = Joi.object({
+    voluntario_id: Joi.string().required(),
+    actividad_id: Joi.string().required(),
+    estado: Joi.string().valid(
+      "Pendiente",
+      "Aprobada",
+      "Rechazada",
+      "Cancelado"
+    ), // Agregar validación para estado
+  });
 
-    const { error } = schema.validate(req.body);
-    if (error) {
-        return res.status(400).json({ message: error.details[0].message });
-    }
-    next();
+  const { error } = schema.validate(req.body);
+  if (error) {
+    return res.status(400).json({ message: error.details[0].message });
+  }
+  next();
 };
 
-router.get('/', getAllInscripciones);
-router.get('/:id', getInscripcionById);
-router.post('/', validateInscripcionData, createInscripcion);
-router.put('/:id', validateInscripcionData, updateInscripcion);
-router.delete('/:id', deleteInscripcion);
+router.get("/", getAllInscripciones);
+router.get("/:id", getInscripcionById);
+router.get("/activity/:id", getInscripcionesByActividadId);
+router.post("/", validateInscripcionData, createInscripcion);
+router.put("/:id", validateInscripcionData, updateInscripcion);
+router.delete("/:id", deleteInscripcion);
 
 export default router;
+
