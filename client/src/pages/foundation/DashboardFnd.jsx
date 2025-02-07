65a97a67c4793746739b91f5471211cb4b0d86df
Mauro Laime <maurol.dev@gmail.com>
2025-02-07 20:28:33 -0300

    Fixed title on dashboard

--- client/src/pages/foundation/DashboardFnd.jsx
+++ client/src/pages/foundation/DashboardFnd.jsx
@@ -8,46 +8,43 @@ import { ROUTES } from "../../routes/index.routes.js";
 import useAuth from "../../hooks/useAuth.js";
 import useActivitiesByFndId from "../../hooks/useActivitiesByFndId.js";
 
-
 function DashboardFnd() {
   const { user } = useAuth();
 
- 
-
   const { loading, activities } = useActivitiesByFndId(user.id);
-  if (!loading) 
-
-  return (
-    <div>
-      <Banner>
-        Crea actividades para que los voluntarios se inscriban en tus propuestas
-      </Banner>
-      <div className="container px-4 pb-10 space-y-6 lg:mx-auto">
-        {/* <IncompleteDashboard /> */}
-        <div className="flex items-center flex-col space-y-6 lg:space-y-0 lg:flex-row-reverse lg:items-start lg:gap-12 lg:-mt-8 bg-neutral-content lg:p-12 lg:rounded-lg">
-          <Link
-            to={ROUTES.FOUNDATION.CREAR_PUBLICACIONES}
-            role="button"
-            className="w-full -mt-5 font-bold text-center lg:mt-0 lg:w-auto btn-primary btn text-primary-content"
-          >
-            <HiMiniPlus /> Crear una nueva actividad
-          </Link>
-          <div className="space-y-4">
-            <TitleDoubleXL>Voluntarios creados</TitleDoubleXL>
-            <p className="text-neutral lg:text-lg">
-              Desde aquí podrás revisar o actualizar los detalles de todas tus
-              actividades publicadas, enviar mensajes a los voluntarios
-              inscriptos o cerrar las inscripciones manualmente.
-            </p>
+  if (!loading)
+    return (
+      <div>
+        <Banner>
+          Crea actividades para que los voluntarios se inscriban en tus
+          propuestas
+        </Banner>
+        <div className="container px-4 pb-10 space-y-6 lg:mx-auto">
+          {/* <IncompleteDashboard /> */}
+          <div className="flex flex-col items-center space-y-6 lg:space-y-0 lg:flex-row-reverse lg:items-start lg:gap-12 lg:-mt-8 bg-neutral-content lg:p-12 lg:rounded-lg">
+            <Link
+              to={ROUTES.FOUNDATION.CREAR_PUBLICACIONES}
+              role="button"
+              className="w-full -mt-5 font-bold text-center lg:mt-0 lg:w-auto btn-primary btn text-primary-content"
+            >
+              <HiMiniPlus /> Crear una nueva actividad
+            </Link>
+            <div className="space-y-4">
+              <TitleDoubleXL>Voluntariados creados</TitleDoubleXL>
+              <p className="text-neutral lg:text-lg">
+                Desde aquí podrás revisar o actualizar los detalles de todas tus
+                actividades publicadas, enviar mensajes a los voluntarios
+                inscriptos o cerrar las inscripciones manualmente.
+              </p>
+            </div>
           </div>
+          {!loading && (
+            <ActivitiesListFnd loading={loading} activity={activities} />
+          )}
         </div>
-        {!loading && (
-          <ActivitiesListFnd loading={loading} activity={activities} />
-        )}
+        <Footer />
       </div>
-      <Footer />
-    </div>
-  );
+    );
 }
 
 export default DashboardFnd;
